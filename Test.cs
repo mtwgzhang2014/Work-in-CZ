673fa3f0d5c31be79aceeb5517135ecd7760a0b0
mtwgzhang2014 <*mtwgzhang=20140701*>
2015-10-28 09:48:47 +0800

    modified test

---
+++ Test.cs
@@ -0,0 +1,14 @@
+using system;
+
+namespace test
+{
+	public static class example
+	{
+		public static void Main()
+		{
+			console.writeline("Hello world!");
+			console.writeline("Hello MT!");
+			console.readKey();
+		}
+	}
+}
