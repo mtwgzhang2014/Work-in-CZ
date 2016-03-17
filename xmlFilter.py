1881a7f4a5c336ae94ede999d72a003630bbd329
Mason Zhang <wenge_zhang2011@163.com>
2016-03-18 00:25:18 +0800

    Update xmlFilter

--- xmlFilter.py
+++ xmlFilter.py
@@ -12,40 +12,80 @@ __author__ = 'Mason Zhang'
 
 import re
 
-filePath = r'C:\Users\zhang-384\Desktop\7284080391-6-20150320121642-MeasData.xml'
-# 获取纯角差数据
-pattern = re.compile("Hone_Depth2(.*?)EndTime:", re.S) 
-# re.S 使 . 匹配包括换行在内的所有字符
-# 在此可以避免换行符\n不能被匹配
+# filePath = r'C:\Users\Administrator\Desktop\7283523086-6-20150106124217-MeasData.xml'
 
-with open(filePath, 'r') as xmlfile:
-	tmpXml = xmlfile.read()
-	# re.search 扫描整个字符串并返回第一个成功的匹配
-	# 而re.match 尝试从字符串的起始位置匹配一个模式，
-	# 如果不是起始位置匹配成功的话，match()就返回none
-	tmpData = re.search(pattern, tmpXml, flags = 0)
-	# if tmpData != None:
-	# 	print(tmpData.group(1))
-	# else:
-	# 	print(tmpData)
 
-if tmpData!=None:
-	dataList = tmpData.group(1).split()
-	g = iter(dataList)
-	if len(dataList) % 12 == 0:
-		rowNum = int(len(dataList)/12)
-		# 下面是两种将一维List转变为二维List的语句
-		# 1. 采用迭代器结果逐个赋值到二维List中，[[0 for..] for..]二维数组
-		# 2. 利用zip函数的特性，但结果为List中包含tuple的形式
-		dataArray = [[next(g) for _ in range(12)] for _ in range(rowNum)]
-		# * unpack the arguments out of a list or tuple
-		#dataArray = list(zip(*[iter(dataList)]*12))
-	elif len(dataList) % 16 == 0:
-		pass
-	else:
-		pass
-	# 如何处理即可以被12整除又可以被16整除的情况
+def obtainKeyData(filePath):
+	# 关键数据 L_error, T_error, up_pos, other_pos, right_pos, left_pos
+	keyData = [0]*6
+	# 获取纯角差数据
+	pattern16 = re.compile("X4(.*?)EndTime:", re.S)
+	pattern12= re.compile("Hone_Depth2(.*?)EndTime:", re.S) 
+	# re.S 使 . 匹配包括换行在内的所有字符
+	# 在此可以避免换行符\n不能被匹配
 
-	# dataArray[i][j] for i is row and j is column
-	print(dataArray)
-	print(dataArray[14][11])
+	with open(filePath, 'r') as xmlfile:
+		tmpXml = xmlfile.read()
+		# re.search 扫描整个字符串并返回第一个成功的匹配
+		# 而re.match 尝试从字符串的起始位置匹配一个模式，
+		# 如果不是起始位置匹配成功的话，match()就返回none
+		tmpData = re.search(pattern16, tmpXml)
+		if tmpData == None:
+			tmpData = re.search(pattern12, tmpXml)
+		# if tmpData != None:
+		# 	print(tmpData.group(1))
+		# else:
+		# 	print(tmpData)
+
+	if tmpData!=None:
+		dataList = tmpData.group(1).split()
+		g = iter(dataList)
+		if len(dataList) % 12 == 0 and float(dataList[15]) > 1:
+			rowNum = int(len(dataList)/12)
+			# 下面是两种将一维List转变为二维List的语句
+			# 1. 采用迭代器结果逐个赋值到二维List中，[[0 for..] for..]二维数组
+			# 2. 利用zip函数的特性，但结果为List中包含tuple的形式
+			dataArray = [[next(g) for _ in range(12)] for _ in range(rowNum)]
+			# * unpack the arguments out of a list or tuple
+			#dataArray = list(zip(*[iter(dataList)]*12))
+		elif len(dataList) % 16 == 0 and float(dataList[15]) < 1:
+			rowNum = int(len(dataList)/16)
+			dataArray = [[next(g) for _ in range(16)] for _ in range(rowNum)]
+		else:
+			return 'Bad data!'
+		# 如何处理即可以被12整除又可以被16整除的情况
+
+		# dataArray[i][j] for i is row and j is column
+		# print(dataArray)
+		#print('L_error = %s, T_error = %s' % (dataArray[0][4], dataArray[0][5]))
+		# L_error
+		keyData[0] = int(dataArray[0][4])
+		# T_error
+		keyData[1] = int(dataArray[0][5])
+		for i in range(len(dataArray)):
+			if dataArray[i][6] == '0':
+				keyData[2] += int(dataArray[i][8])
+			elif dataArray[i][6] == '1':
+				keyData[3] += int(dataArray[i][8])
+			elif dataArray[i][6] == '2':
+				keyData[4] += int(dataArray[i][8])
+			elif dataArray[i][6] == '3':
+				keyData[5] += int(dataArray[i][8])
+			else:
+				raise ValueError('The original xml has some mistake!')
+			if dataArray[i][9] == '0':
+				keyData[2] += int(dataArray[i][11])
+			elif dataArray[i][9] == '1':
+				keyData[3] += int(dataArray[i][11])
+			elif dataArray[i][9] == '2':
+				keyData[4] += int(dataArray[i][11])
+			elif dataArray[i][9] == '3':
+				keyData[5] += int(dataArray[i][11])
+			else:
+				raise ValueError('The original xml has some mistake!')
+		#print(keyData)
+		return keyData
+
+if __name__ == "__main__":
+	obtainKeyData(filePath)
+
