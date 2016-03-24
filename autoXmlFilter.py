a11298b3710ee81c564498a8edb4dd86d22d4b2a
mtwgzhang2014 <wenge_zhang2011@163.com>
2016-03-24 10:59:12 +0800

    for 16rows data

--- autoXmlFilter.py
+++ autoXmlFilter.py
@@ -9,11 +9,12 @@ import os
 import os.path
 from xmlFilter import obtainKeyData
 
-targetDir = r'H:\selectXml'
+#targetDir = r'H:\selectXml'
+targetDir = r'C:\Users\zhang-384\Desktop\My Works\00 OST\2015\2. 钢自动角差项目\selectXml'
 
 for parent, dirs, filenames in os.walk(targetDir):
 		for file in filenames:
-			#print(file)
+			print(file)
 			targetFile = os.path.join(targetDir, file)
 			alist = obtainKeyData(targetFile)
 			if alist != 'Bad data!':
