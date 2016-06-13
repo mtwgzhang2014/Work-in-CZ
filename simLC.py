19945934946a35385d261d7c3390a09b28125852
mtwgzhang2014 <wenge_zhang2011@163.com>
2016-06-13 13:57:32 +0800

    add simLC

---
+++ simLC.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# load cell simulation
+
+import numpy as np 
+from array import array
+from enum import Enum,unique
+
+@unique
+class HonePos(Enum):
+	Up = 0
+	Other = 1
+	Right = 2
+	Left = 3
+
+class simLC:
+	def __init__(self,initUp,initRight,initLeft,noiseSigma):
+		self.Up = initUp
+		self.Right = initRight
+		self.Left = initLeft
+		self.Other = initLeft+initUp-initRight
+		self.initLongError = np.round((self.Right - self.Up)/self.Right*1e6)
+		self.initTranError = np.round((self.Right - self.Left)/self.Right*1e6)
+		self.Noise = np.round(noiseSigma * np.random.randn(4,1))
+		self.cornerTheta = np.zeros((4,4))
+		self._tmpRecord = array("i")
+		self.adjRecord = np.zeros((1,4))
+		self._tmpStepRecord = array("i")
+		self._honeStepRecord = np.array([])
+		self._tmpPosRecord = array("i")
+		self._honePosRecord = np.array([])
+
+	@property
+	def LongError(self):
+	    return np.round((self.Right - self.Up)/self.Right*1e6)
+	@property
+	def TranError(self):
+	    return np.round((self.Right - self.Left)/self.Right*1e6)
+
+	def _calcHoneAdj(self,honeStep,honePos):
+		if honePos == 0: # POS Up
+			self.cornerTheta[0,:] = np.array([2.0572e-7, 	0.001046,	0,	80])	# Up corner parameter			
+			self.cornerTheta[2,:] = np.array([1.4275e-7,	0.0007437,	0,	54])	# Right corner parameter
+			self.cornerTheta[3,:] = np.array([6.3343e-8, 	-4.3617e-5,	0,	37])	# Left corner parameter
+		elif honePos == 1: # POS Other
+			self.cornerTheta[0,:] = np.array([9.7984e-8,	0.0009047,	0,	-54])
+			self.cornerTheta[2,:] = np.array([-3.9565e-8,	0.0003041,	0,	-1])
+			self.cornerTheta[3,:] = np.array([3.0545e-7,	0.001797,	0,	-14])
+		elif honePos == 2: # POS Right
+			self.cornerTheta[0,:] = np.array([-1.1985e-8,	0.0008786,	0,	311])
+			self.cornerTheta[2,:] = np.array([5.1397e-8,	0.001171,	0,	355])
+			self.cornerTheta[3,:] = np.array([2.8902e-8,	0.0001329,	0,	306])			
+		elif honePos == 3: # POS Left
+			self.cornerTheta[0,:] = np.array([-6.1081e-8,	-6.3095e-5,	0,	27])
+			self.cornerTheta[2,:] = np.array([1.3651e-8,	0.0001839,	0,	59])
+			self.cornerTheta[3,:] = np.array([1.3890E-7,	0.0008259,	0,	2])
+		else:
+			raise ValueError("honePos value %d invalid!", honePos)
+
+		stepVector = np.array([pow(honeStep,3),pow(honeStep,2),honeStep,1])	
+		adjRight = np.round(np.dot(stepVector,self.cornerTheta[2,:].transpose())+self.Noise[2])
+		adjLeft = np.round(np.dot(stepVector,self.cornerTheta[3,:])+self.Noise[3])
+		adjUp = np.round(np.dot(stepVector,self.cornerTheta[0,:])+self.Noise[0])
+		adjOther = np.round(adjLeft + adjUp - adjRight + self.Noise[1])
+		#print(self.adjUp,self.adjOther,self.adjRight,self.adjLeft)
+		self._tmpRecord.append(adjUp)
+		self._tmpRecord.append(adjOther)
+		self._tmpRecord.append(adjRight)
+		self._tmpRecord.append(adjLeft)
+		self.adjRecord = np.frombuffer(self._tmpRecord, dtype=np.int).reshape(-1,4)
+		#print(self.adjRecord)
+		self._tmpStepRecord.append(honeStep)
+		self._honeStepRecord = np.frombuffer(self._tmpStepRecord,dtype=np.int)
+		self._tmpPosRecord.append(honePos)
+		self._honePosRecord = np.frombuffer(self._tmpPosRecord,dtype=np.int)
+		return np.array([adjUp, adjOther, adjRight, adjLeft])
+
+	def loadCellOutput(self,honeStep,honePos):
+		if not honePos in self._honePosRecord:
+			adjArray = self._calcHoneAdj(honeStep,honePos)
+			self.Up 	+= 	adjArray[HonePos.Up.value]
+			self.Other 	+= 	adjArray[HonePos.Other.value]
+			self.Right 	+= 	adjArray[HonePos.Right.value]
+			self.Left 	+= 	adjArray[HonePos.Left.value]
+		else:
+			prevHP_i =  np.where(self._honePosRecord == honePos)[0][-1] # previous HonePos Index in adjRecord
+			adjArray = self._calcHoneAdj(honeStep,honePos)
+			self.Up 	= self.Up - self.adjRecord[prevHP_i, HonePos.Up.value] + adjArray[HonePos.Up.value]
+			self.Other 	= self.Other - self.adjRecord[prevHP_i, HonePos.Other.value] + adjArray[HonePos.Other.value]
+			self.Right 	= self.Right - self.adjRecord[prevHP_i, HonePos.Right.value] + adjArray[HonePos.Right.value]
+			self.Left 	= self.Left - self.adjRecord[prevHP_i, HonePos.Left.value] + adjArray[HonePos.Left.value]
+		return np.array([self.Up, self.Other, self.Right, self.Left])
+
+if __name__ == "__main__":
+	testLC = simLC(558708, 555806, 558028, 10)
+	print("The initial Long error is %d and the Tran error is %d " % (testLC.initLongError,testLC.initTranError))
+	#print(testLC.calcHoneAdj(1655,2))
+	#print(testLC.calcHoneAdj(860,3))\
+	# first honing
+	testLC.loadCellOutput(1655,2)
+	testLC.loadCellOutput(860,3)
+	# second honing
+	testLC.loadCellOutput(1919,2)
+	testLC.loadCellOutput(1532,3)
+	# third honing
+	testLC.loadCellOutput(2050,2)
+	testLC.loadCellOutput(1705,3)
+
+	testLC.loadCellOutput(190,0)
+	print(testLC.adjRecord)
+	print("The Long error is %d and the Tran error is %d " % (testLC.LongError,testLC.TranError))
+
+
+
