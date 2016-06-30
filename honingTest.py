01a4d2fca4fe44e4b7b0dcbf4e81ffb5e492a3c9
mtwgzhang2014 <wenge_zhang2011@163.com>
2016-06-30 14:42:54 +0800

    add new honing by fitting the model

--- honingTest.py
+++ honingTest.py
@@ -6,15 +6,35 @@
 from simLC import *
 import numpy as np
 from datetime import datetime
+from scipy.optimize import fsolve
 
 # preset #################################################
 # init_Left = 558028;
 # init_Right = 555806; 
 # init_Up = 558708;
 
-init_Left = 649717 - 166812;
-init_Right = 648354 - 166812; 
-init_Up = 649243 - 166812;
+# L:-1975 T:-2288
+# init_Left = 645528 - 166394;
+# init_Right = 644170 - 166394; 
+# init_Up = 644873 - 166394;
+
+# L:
+# init_Left = 648955 - 165638;
+# init_Right = 647852 - 165638; 
+# init_Up = 648804 - 165638
+
+# L:6483 T:3817
+# init_Left = 620805 - 170220;
+# init_Right = 622531 - 170220; 
+# init_Up = 619599 - 170220;
+
+# L:-313 T:5125
+init_Left = 866181 - 87035;
+init_Right = 870195 - 87035; 
+init_Up = 870441 - 87035;
+
+
+
 
 Noise_Sigma = 20
 
@@ -45,21 +65,27 @@ alphaT = 0.005
 ##########################################################
 
 # first honing ###########################################
-tmpData = np.zeros(2)
+tmpData = np.zeros(2, dtype=np.int64)
 
-def simpleFit(alphaL, alphaT, LongError,TranError,order):
+def simpleFit(alphaL, alphaT, LongError, TranError, order):
 	for m in range(2):
 		for n in range(2):
 			if ((-1)**n*alphaL-(-1)**m*alphaL/alphaT*(-1)**n*alphaT) != 0:
-				tmpData[0] = (LongError - (-1)**m*alphaL/alphaT*TranError)/\
-				((-1)**n*alphaL-(-1)**m*alphaL/alphaT*(-1)**n*alphaT)
+				try:
+					tmpData[0] = (LongError - (-1)**m*alphaL/alphaT*TranError)/\
+					((-1)**n*alphaL-(-1)**m*alphaL/alphaT*(-1)**n*alphaT)
+				except Exception as e:
+					print(e)
+					print("1",(-1)**n*alphaL-(-1)**m*alphaL/alphaT*(-1)**n*alphaT)
+					print("2",LongError - (-1)**m*alphaL/alphaT*TranError)
+					print(tmpData[0])
 			else:
 				tmpData[0] = negInf
 			#print("m, n : %d, %d -- %f" % (m,n,tmpData[0]))
 			# select POS1 or POS2 first
 			if tmpData[0] > 0:
 				curHonePos[0] = n + 1 # map the sign parameter n to POS
-				curHoneStep[0] = np.round(tmpData[0]**(1.0/order))
+				curHoneStep[0] = np.round(tmpData[0]**(1.0/order)*0.9)
 				# then POS0 or POS3
 				for k in range(2):
 					tmpData[1] = (TranError-(-1)**n*alphaT*curHoneStep[0]**order)/\
@@ -70,7 +96,7 @@ def simpleFit(alphaL, alphaT, LongError,TranError,order):
 							curHonePos[1] = 3
 						else:
 							curHonePos[1] = 0
-						curHoneStep[1] = np.round(tmpData[1]**(1.0/order))
+						curHoneStep[1] = np.round(tmpData[1]**(1.0/order)*0.9)
 
 simpleFit(alphaL, alphaT, Long[0], Tran[0], 2)
 testLC.loadCellOutput(curHoneStep[0],curHonePos[0])
@@ -101,7 +127,7 @@ def sign(POS,errorDir):
 
 alphaL = (Long[1] - Long[0])/(sign(curHonePos[0],"Long")*curHoneStep[0]**3 + sign(curHonePos[1],"Long")*curHoneStep[1]**3)
 alphaT = (Tran[1] - Tran[0])/(sign(curHonePos[0],"Tran")*curHoneStep[0]**3 + sign(curHonePos[1],"Tran")*curHoneStep[1]**3)
-print(alphaL,alphaT)
+#print(alphaL,alphaT)
 
 simpleFit(alphaL, alphaT, Long[0], Tran[0], 3)
 testLC.loadCellOutput(curHoneStep[0],curHonePos[0])
@@ -111,20 +137,97 @@ Long.append(testLC.LongError)
 Tran.append(testLC.TranError)
 print("Long Error: %d; Tran Error: %d " % (Long[-1],Tran[-1]))
 
+#####
+# if curPOS == prePOS: 
+# 	Long[cur]-Long[cur-1]
+# else:
+#	Long[cur]-Long[0] or Long[cur]-Long[lastFind]
 
-#simpleFit(testLC.LongError,testLC.TranError,3)
+alphaL = (Long[2] - Long[0])/(sign(curHonePos[0],"Long")*curHoneStep[0]**3 + sign(curHonePos[1],"Long")*curHoneStep[1]**3)
+alphaT = (Tran[2] - Tran[0])/(sign(curHonePos[0],"Tran")*curHoneStep[0]**3 + sign(curHonePos[1],"Tran")*curHoneStep[1]**3)
+#print(alphaL,alphaT)
+simpleFit(alphaL, alphaT, Long[0], Tran[0], 3)
+testLC.loadCellOutput(curHoneStep[0],curHonePos[0])
+testLC.loadCellOutput(curHoneStep[1],curHonePos[1])
+print("Honing: %d POS%d; %d POS%d" % (curHoneStep[0],curHonePos[0],curHoneStep[1],curHonePos[1]))
+Long.append(testLC.LongError)
+Tran.append(testLC.TranError)
+print("Long Error: %d; Tran Error: %d " % (Long[-1],Tran[-1]))
 
 
-#testLC.loadCellOutput(1344,2)
-#testLC.loadCellOutput(898,3)
-#print("")
-##########################################################
-# second honing
-# testLC.loadCellOutput(1919,2)
-# testLC.loadCellOutput(1532,3)
-# # third honing
-# testLC.loadCellOutput(2050,2)
-# testLC.loadCellOutput(1705,3)
 
-# testLC.loadCellOutput(190,0)
-#print(testLC.adjRecord)
+# alphaL = (Long[3] - Long[0])/(sign(curHonePos[0],"Long")*curHoneStep[0]**3 + sign(curHonePos[1],"Long")*curHoneStep[1]**3)
+# alphaT = (Tran[3] - Tran[0])/(sign(curHonePos[0],"Tran")*curHoneStep[0]**3 + sign(curHonePos[1],"Tran")*curHoneStep[1]**3)
+# #print(alphaL,alphaT)
+
+# simpleFit(alphaL, alphaT, Long[0], Tran[0], 3)
+# testLC.loadCellOutput(curHoneStep[0],curHonePos[0])
+# testLC.loadCellOutput(curHoneStep[1],curHonePos[1])
+# print("Honing: %d POS%d; %d POS%d" % (curHoneStep[0],curHonePos[0],curHoneStep[1],curHonePos[1]))
+# Long.append(testLC.LongError)
+# Tran.append(testLC.TranError)
+# print("Long Error: %d; Tran Error: %d " % (Long[-1],Tran[-1]))
+
+
+
+# alphaL = (Long[4] - Long[0])/(sign(curHonePos[0],"Long")*curHoneStep[0]**3 + sign(curHonePos[1],"Long")*curHoneStep[1]**3)
+# alphaT = (Tran[4] - Tran[0])/(sign(curHonePos[0],"Tran")*curHoneStep[0]**3 + sign(curHonePos[1],"Tran")*curHoneStep[1]**3)
+# #print(alphaL,alphaT)
+
+# simpleFit(alphaL, alphaT, Long[0], Tran[0], 3)
+# testLC.loadCellOutput(curHoneStep[0],curHonePos[0])
+# testLC.loadCellOutput(curHoneStep[1],curHonePos[1])
+# print("Honing: %d POS%d; %d POS%d" % (curHoneStep[0],curHonePos[0],curHoneStep[1],curHonePos[1]))
+# Long.append(testLC.LongError)
+# Tran.append(testLC.TranError)
+# print("Long Error: %d; Tran Error: %d " % (Long[-1],Tran[-1]))
+
+# testLC.loadCellOutput(1500,1)
+# testLC.loadCellOutput(1700,0)
+
+testLC.loadCellOutput(1020,1)
+testLC.loadCellOutput(1600,3)
+print("Honing: %d POS%d; %d POS%d" % (curHoneStep[0],curHonePos[0],curHoneStep[1],curHonePos[1]))
+Long.append(testLC.LongError)
+Tran.append(testLC.TranError)
+print("Long Error: %d; Tran Error: %d " % (Long[-1],Tran[-1]))
+
+#############################################################################################################
+# new test
+
+#print(testLC.honeStepRecord)
+# honeStep4Tran = np.zeros([2,testLC.honeStepRecord.shape[0],4])
+# honeStep4Long = np.zeros([2,testLC.honeStepRecord.shape[0],4])
+
+# for i in range(testLC.honeStepRecord.shape[0]):
+# 	for j in range(4):
+# 		honeStep4Tran[0,i,j] = sign(j,"Tran")*testLC.honeStepRecord[i,j]**3
+# 		honeStep4Tran[1,i,j] = sign(j,"Tran")*testLC.honeStepRecord[i,j]**2
+# 		honeStep4Long[0,i,j] = sign(j,"Long")*testLC.honeStepRecord[i,j]**3
+# 		honeStep4Long[1,i,j] = sign(j,"Long")*testLC.honeStepRecord[i,j]**2
+
+
+# alphaT = np.linalg.lstsq(sum(honeStep4Tran.transpose()), np.frombuffer(Tran[1:]-Tran[0], dtype=float))[0]
+# alphaL = np.linalg.lstsq(sum(honeStep4Long.transpose()), np.frombuffer(Long[1:]-Long[0], dtype=float))[0]
+# #print(honeStep4Tran)
+# print(alphaT)
+# print(alphaL)
+
+# honePos = curHonePos
+
+# def fModel(step):
+# 	s1 = step[0]
+# 	s2 = step[1]
+# 	calTran = alphaT[0]*(sign(honePos[0],"Tran")*s1**3+sign(honePos[1],"Tran")*s2**3) + alphaT[1]*(sign(honePos[0],"Tran")*s1**2+sign(honePos[1],"Tran")*s2**2) + Tran[0]
+# 	calLong = alphaL[0]*(sign(honePos[0],"Long")*s1**3+sign(honePos[1],"Long")*s2**3) + alphaL[1]*(sign(honePos[0],"Long")*s1**2+sign(honePos[1],"Long")*s2**2) + Long[0]
+# 	return [calTran, calLong]
+
+# curHoneStep = np.round(fsolve(fModel, curHoneStep))
+# print("result is: ", curHoneStep)
+
+# testLC.loadCellOutput(curHoneStep[0],curHonePos[0])
+# testLC.loadCellOutput(curHoneStep[1],curHonePos[1])
+# print("Honing: %d POS%d; %d POS%d" % (curHoneStep[0],curHonePos[0],curHoneStep[1],curHonePos[1]))
+# Long.append(testLC.LongError)
+# Tran.append(testLC.TranError)
+# print("Long Error: %d; Tran Error: %d " % (Long[-1],Tran[-1]))
